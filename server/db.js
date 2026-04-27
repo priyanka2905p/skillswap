88827dae27a06dfe08935c4a60d5cee6211fa922
Priyankapawar <priyankapawarp0477@gmail.com>
2026-04-28 05:21:42 +0530

    fix mysql connection

--- server/db.js
+++ server/db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "tiger",
-  database: "skillswap"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
-db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected...");
+db.connect((err) => {
+  if (err) {
+    console.log("DB connection failed ❌", err);
+  } else {
+    console.log("DB connected successfully ✅");
+  }
 });
 
 module.exports = db;
