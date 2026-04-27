6b57719efa11e6f1f2cf0dc7bd86c51130b93350
Priyankapawar <priyankapawarp0477@gmail.com>
2026-04-28 05:08:10 +0530

    fix mysql connection

--- server/db.js
+++ server/db.js
@@ -1,22 +1,15 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: process.env.mysql.railway.internal,        // Railway MYSQLHOST
-  user: process.env.root,        // Railway MYSQLUSER
-  password: process.env.FncdOyvkUXhKoQaoWBOLQqPCZlMyVOdx,// Railway MYSQLPASSWORD
-  database: process.env.railway,    // Railway MYSQLDATABASE
-  ssl: {
-    rejectUnauthorized: true
-  }
+  host: "localhost",
+  user: "root",
+  password: "tiger",
+  database: "skillswap"
 });
 
-// Connect check (optional but useful)
-db.connect((err) => {
-  if (err) {
-    console.log("❌ DB Connection Failed:", err);
-  } else {
-    console.log("✅ Connected to Railway MySQL");
-  }
+db.connect(err => {
+  if (err) throw err;
+  console.log("MySQL Connected...");
 });
 
 module.exports = db;
